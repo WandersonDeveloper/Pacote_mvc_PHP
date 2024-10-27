60eb4d391bcd6e0bae8aca1d439c33f61ab61fac
Wanderson Felipe de Oliveira <41800274+WandersonDeveloper@users.noreply.github.com>
2024-10-27 00:00:17 -0400

    phpmvc

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wanderson-developer/pacote-mvc-php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b76bd56f9780d5032d14e7aa0ce42f2d868f272b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wanderson-developer/pacote-mvc-php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b76bd56f9780d5032d14e7aa0ce42f2d868f272b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
